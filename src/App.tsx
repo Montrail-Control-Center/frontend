68594cafe2104f9deb81b5670ab4ee5a28a05c2e
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2023-03-06 16:09:55 +0100

    confirm style setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Homepage from './pages/Homepage/Homepage';
+import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Montrail International.</h1>
-      </header>
+    <div className='container'>
+      <Homepage/>
     </div>
   );
 }
